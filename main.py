bdc55a669853c9f8d44139134d703cf541185dd9
David Edwards <david_edwards4@my.vcccd.edu>
2024-06-30 15:47:29 -0700

    Calculating an Average

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ def main():
     total 
     average
     """
-
+    val1 = int(input('Enter first value:  '))
+    val2 = int(input('Enter second value:  '))
+    val3 = int(input('Enter third value:  '))
+    sum = val1 + val2
+    total = sum + val3
+    average = (total/3)
+    
+    print (f"values : {val1, val2, val3}")
+    print(f"total : {total:.2f}")
+    print (f"average : {average:.2f}")
+    
     ########################################
     # Do not delete the return statement
     ########################################
